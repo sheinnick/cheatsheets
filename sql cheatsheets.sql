fdb31dcec4a2d6c420c30fc1e9fa1a5c32d79b93
Nikita Shein <shein.nikita@gmail.com>
2021-04-05 09:46:56 +0300

    BQ info about tables

--- sql cheatsheets.sql
+++ sql cheatsheets.sql
@@ -206,3 +206,22 @@ SELECT
  * EXCEPT(check_option)
 FROM
  `base.dataset.INFORMATION_SCHEMA.VIEWS`
+
+
+ /* BQ info about tables */
+SELECT table_id,
+    DATE(TIMESTAMP_MILLIS(creation_time)) AS creation_date,
+    DATE(TIMESTAMP_MILLIS(last_modified_time)) AS last_modified_date,
+    row_count,
+    size_bytes,
+    CASE
+        WHEN type = 1 THEN 'table'
+        WHEN type = 2 THEN 'view'
+        WHEN type = 3 THEN 'external'
+        ELSE '?'
+    END AS type,
+    TIMESTAMP_MILLIS(creation_time) AS creation_time,
+    TIMESTAMP_MILLIS(last_modified_time) AS last_modified_time,
+    dataset_id,
+    project_id
+FROM `base.dataset.__TABLES__`
